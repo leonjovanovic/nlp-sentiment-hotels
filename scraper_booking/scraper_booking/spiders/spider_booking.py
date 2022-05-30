a0d99bb2aa34f8d4cb9915e1f27a94ff27a70841
leonjovanovic <leonjovanovic1996@gmail.com>
2022-05-30 18:07:17 +0200

    Update scraper

--- scraper_booking/scraper_booking/spiders/spider_booking.py
+++ scraper_booking/scraper_booking/spiders/spider_booking.py
@@ -1,7 +1,5 @@
 import scrapy
 
-
-
 class BookingSpider(scrapy.Spider):
     name = "spider_booking"
 
@@ -14,14 +12,18 @@ class BookingSpider(scrapy.Spider):
 
     def parse(self, response):
         i = 0
-        page = response.url.split("/")[-1][:8]
-        filename = f'hotels-{page}.html'
-        with open(filename, 'wb') as f:
-            f.write(response.body)
         for selector in response.xpath('//div[@data-testid="property-card"]//div//div//div//a'):
             if 'aria-hidden' in selector.attrib.keys():
                 print(i)
                 href = selector.attrib['href']
-                print(href)
                 i += 1
-                yield response.follow(href, self.parse, headers = {'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_2) AppleWebKit/601.3.9 (KHTML, like Gecko) Version/9.0.2 Safari/601.3.9'})
+                yield response.follow(href, self.parse_hotel, headers = {'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_2) AppleWebKit/601.3.9 (KHTML, like Gecko) Version/9.0.2 Safari/601.3.9'})
+        
+        if '&offset=' not in response.url:
+            pagination_offset = 25
+            for i in range(1, 40):
+                new_url = response.url + '&offset=' + str(pagination_offset * i)
+                yield response.follow(new_url, self.parse, headers = {'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_2) AppleWebKit/601.3.9 (KHTML, like Gecko) Version/9.0.2 Safari/601.3.9'})
+
+    def parse_hotel(self, response):
+        print(response.url.split("/")[-1][:15])
