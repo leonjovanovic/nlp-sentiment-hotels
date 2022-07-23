6c58c3c70aab4df64d94cf795594e70f09467907
leonjovanovic <leonjovanovic1996@gmail.com>
2022-07-23 16:47:50 +0200

    Revert "started cross validation"
    This reverts commit 389b88e39bcd986b3b44d167c41cc66a7e223149.

--- models/bayes_classificator.py
+++ models/bayes_classificator.py
@@ -1,8 +1,13 @@
 import pandas as pd
 import numpy as np
 from math import log
-from preprocess import prepare_dataset
-from utils import ModelType
+import utils
+
+
+class ModelType:
+    CATEGORY = 0
+    SENTIMENT = 1
+    BOTH = 2
 
 
 class NaiveBayes:
@@ -32,7 +37,7 @@ class NaiveBayes:
 
 
     def train(self, input_data, output_data):
-        df_train = prepare_dataset(input_data, output_data, self.type)
+        df_train = self.prepare_dataset(input_data, output_data)
         self.calc_probabilities_per_class(df_train)
 
 
@@ -47,9 +52,22 @@ class NaiveBayes:
 
     
     def test(self, input_data, output_data):
-        df_test = prepare_dataset(input_data, output_data, self.type)
+        df_test = self.prepare_dataset(input_data, output_data)
         df_test['prediction'] = pd.DataFrame(df_test.apply(lambda x: self.compute(x[0]), axis=1))
-        return len(df_test[df_test['prediction'] != df_test[df_test.columns[-2]]])/len(df_test)
+        return df_test
+
+
+    def prepare_dataset(self, input_data, output_data):
+        input_data = pd.DataFrame(input_data)
+        if self.type == ModelType.CATEGORY:
+            return pd.concat([input_data, output_data.apply(lambda x: 1 if x == 2 else x)], axis=1)
+        elif self.type == ModelType.SENTIMENT:
+            data = pd.concat([input_data, output_data], axis=1)
+            data = data[data[data.columns[-1]] != 0]
+            data[data.columns[-1]] = data[data.columns[-1]].apply(lambda x: x - 1)
+            data.reset_index(drop=True, inplace=True)
+            return data
+        return pd.concat([input_data, output_data], axis=1)
 
 
 class NaiveBayesCombined:
@@ -63,8 +81,7 @@ class NaiveBayesCombined:
 
     def test(self, input_data, output_data):
         prediction = pd.DataFrame(input_data).apply(lambda x: self.compute(x[0]), axis=1)
-        df_test = pd.concat([input_data.rename('hotel_review'), output_data, prediction.rename('prediction')], axis=1)
-        return len(df_test[df_test['prediction'] != df_test[df_test.columns[-2]]])/len(df_test)
+        return pd.concat([input_data.rename('hotel_review'), output_data, prediction.rename('prediction')], axis=1)
 
     def compute(self, review):
         if self.bayes_category.compute(review):
--- models/logistic_regression_classificator.py
+++ models/logistic_regression_classificator.py
@@ -1,13 +1,12 @@
 import numpy as np
-from preprocess import prepare_dataset
-from utils import ModelType
 
 class LogisticRegression:
-    def __init__(self, type: ModelType) -> None:
+    def __init__(self) -> None:
         self.W = None
-        self.type = type
-        if self.type == ModelType.BOTH:
-            raise Exception('Logistic regression is a binary classificator!')
+
+    def prepare_dataset(self, input_data, output_data):
+        # Verovatno cemo izvuci prepare_dataset iz naive_bayes obzirom da nam treba ista stvar i za LogReg
+        return 0
 
 
     def split_data_input_output(self, data):
@@ -15,7 +14,10 @@ class LogisticRegression:
 
 
     def calculate_hypotesis(self, X, Y):
-        # Formula: h(x) = 1 / (1 + e^-(w0*1 + w1*x1 + w2*x2 + ... + wn*xn)) = 1 / (1 + e^-W*X^T) = 1 / (1 + e^-Z) = H
+        # Formula: h(x) = 1 / (1 + e^-(w0 + w1*x1 + w2*x2 + ... + wn*xn)) = 1 / (1 + e^-W*(1,X)^T) = 1 / (1 + e^-Z) = H
+        # Adding ones as first element in every row for future multiplication with w0 (bias term)
+        ones_bias_term = np.ones([mini_batch_size, 1])
+        X = np.hstack(ones_bias_term, X) # mini_batch_size x 1(one) + num_of_features
         # Multiplying parameters with input features which need to be transponed for vector multiplication to work
         Z = np.dot(self.W, X.transpose()) # mini_batch_size x 1
         # Applying Sigmoid function to get hypotesis
@@ -24,7 +26,7 @@ class LogisticRegression:
 
 
     def calculate_cost_function(self, H, Y):
-        L = -Y * np.log(H) - (1 - Y) * np.log(1 - H) # mini_batch_size x 1
+        L = Y * np.log(H) + (1 - Y) * np.log(1 - H) # mini_batch_size x 1
         J = -np.mean(L) # 1 x 1
         return J
 
@@ -37,11 +39,8 @@ class LogisticRegression:
 
     def compute(self, data):
         X, Y = self.split_data_input_output(data)
-        # Adding ones as first element in every row for future multiplication with w0 (bias term)
-        ones_bias_term = np.ones([mini_batch_size, 1])
-        X = np.hstack(ones_bias_term, X) # mini_batch_size x 1(one) + num_of_features
         H = self.calculate_hypotesis(X, Y)
-        J = self.calculate_cost_function(H, Y)
+        J = self.calculate_cost_function()
         return X, Y, H, J
 
 
@@ -52,17 +51,15 @@ class LogisticRegression:
         return J
 
 
-    def train(self, train_input_data, train_output_data, validation_input_data = None, validation_output_data = None):
+    def train(self, train_input_data, train_output_data, validation_input_data, validation_output_data):
         self.W = np.zeros([1, len(train_input_data.columns) + 1]) # Weights for Logistic regression 1 x num_of_features + 1 for bias term
-        train_dataset = prepare_dataset(train_input_data, train_output_data, self.type).to_numpy()
-        validation_dataset = None
-        if validation_input_data:
-            validation_dataset = prepare_dataset(validation_input_data, validation_output_data, self.type).to_numpy()
+        np_train = self.prepare_dataset(train_input_data, train_output_data).to_numpy()
+        np_validation = self.prepare_dataset(validation_input_data, validation_output_data).to_numpy()
 
         for iteration in range(num_of_iterations):        
-            for mini_batch_index in range(0, train_dataset.shape[0], mini_batch_size):
-                J_train = self.train_mini_batch(train_dataset, mini_batch_index)
-                if  validation_dataset: _, _, H_validation, J_validation = self.compute(validation_dataset)
+            for mini_batch_index in range(0, np_train.shape[0], mini_batch_size):
+                j_train = self.train_mini_batch(np_train, mini_batch_index)
+                J_validation = self.compute(np_validation)
 
 
 
