c48ca34fdb27e9b31fb3a073510d894907880e48
Anja Lukic <anjalukic97@gmail.com>
2022-08-19 16:46:25 +0200

    Add calibration script

--- annotating_data.py
+++ annotating_data.py
@@ -1,21 +1,35 @@
 import pandas as pd
 
 
-def save_progress(df):
+def save_progress(df, calibrate):
     # save progress to file
     out = df.to_json(orient='index', indent=4, force_ascii=False)
-    with open(f'data/annotated_reviews_lang_1.json', 'w', encoding='utf-8') as f:
-        f.write(out)
+    if not calibrate:
+        with open(f'data/annotated_reviews_lang_2.json', 'w', encoding='utf-8') as f:
+            f.write(out)
+    else:
+        with open(f'data/calibration_reviews.json', 'w', encoding='utf-8') as f:
+            f.write(out)
+        pass
 
 
-def start_annotate():
+
+def start_annotate(calibrate=False):
     # load reviews
-    df = pd.read_json(f'data/annotated_reviews_lang_1.json', orient='index')
+    df1 = pd.read_json(f'data/annotated_reviews_lang_2.json', orient='index')
+    if calibrate:
+        df2 = pd.read_json('data/annotated_reviews_lang_1.json', orient='index')
+        df = pd.concat([df1.iloc[:125, :], df2.iloc[:125, :]])
+        df.reset_index(drop=True, inplace=True)
+    else:
+        df = df1
     entities = ['amenities', 'location', 'cleanliness', 'staff']
 
     index = 0
     while (df.loc[index][-4:] != ['n/a']*4).all():
         index += 1
+        if index == len(df.index):
+            break
 
     while index < len(df.index):
         print(f"""{index}: {df['hotel_review'].loc[index]}""")
@@ -25,7 +39,7 @@ def start_annotate():
         if options == 'd':
             df.loc[index][-4:] = 'skipped'
             index += 1
-            save_progress(df)
+            save_progress(df, calibrate)
             continue
         elif options == 'exit':
             break
@@ -39,10 +53,11 @@ def start_annotate():
                 value = input(f'{entity}? ')
             df.loc[index][-4+i] = value
 
-        save_progress(df)
+        save_progress(df, calibrate)
 
         index += 1
 
-
+# start_annotate(calibrate=True)
 start_annotate()
+
 
